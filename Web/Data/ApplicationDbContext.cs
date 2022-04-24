10cde5c257cb1ffe76089e79aebb80b97404fde7
RajaKalavala <kalavala.raja@siemens-healthineers.com>
2022-04-24 13:24:15 +0530

    Create DB using Entity Framework

--- Web/Data/ApplicationDbContext.cs
+++ Web/Data/ApplicationDbContext.cs
@@ -3,7 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Microsoft.EntityFrameworkCore;
+
+using Web.Models;
+
 namespace Web.Data
 {
-    public class ApplicationDbContext : DbContext {}
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext( DbContextOptions<ApplicationDbContext> options ) : base( options ) {}
+
+        public DbSet<Category> Category { get; set; }
+    }
 }
