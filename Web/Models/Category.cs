652b1ede3621330659de01b159525290aded73fb
RajaKalavala <kalavala.raja@siemens-healthineers.com>
2022-04-24 14:44:30 +0530

    Validation of Form

--- Web/Models/Category.cs
+++ Web/Models/Category.cs
@@ -12,9 +12,12 @@ namespace Web.Models
         [Key]
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
         [DisplayName( "Display Order" )]
+        [Required]
+        [Range( 1, int.MaxValue, ErrorMessage = "Display Order must be greater than 0" )]
         public int DisplayOrder { get; set; }
     }
 }
