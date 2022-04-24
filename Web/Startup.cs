4ef10dbfcbbc07936a3c62fe1cda5e6df028c530
RajaKalavala <kalavala.raja@siemens-healthineers.com>
2022-04-24 12:15:35 +0530

    Add DbContext to ConfigurationService

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -10,11 +10,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Microsoft.EntityFrameworkCore;
+
+using Web.Data;
+
 namespace Web
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup( IConfiguration configuration )
         {
             Configuration = configuration;
         }
@@ -22,24 +26,28 @@ namespace Web
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices( IServiceCollection services )
         {
+            services.AddDbContext<ApplicationDbContext>( options =>
+                                                                 options.UseSqlServer(
+                                                                                      Configuration.GetConnectionString( "DefaultConnection" ) ) );
             services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure( IApplicationBuilder app, IWebHostEnvironment env )
         {
-            if (env.IsDevelopment())
+            if( env.IsDevelopment() )
             {
                 app.UseDeveloperExceptionPage();
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler( "/Home/Error" );
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -47,12 +55,12 @@ namespace Web
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
+            app.UseEndpoints( endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
+                                             name : "default",
+                                             pattern : "{controller=Home}/{action=Index}/{id?}" );
+            } );
         }
     }
 }
